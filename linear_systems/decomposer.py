2ca1e7d5cfb881df252c76fb41abb1e81f65bdda
Akshay <akshayarora1909@gmail.com>
2023-03-08 00:13:44 -0600

    Update QR Decompose

--- linear_systems/decomposer.py
+++ linear_systems/decomposer.py
@@ -1,10 +1,12 @@
 from .utils.decompose_utils import Decomposer
 
 def decompose(method_name,method_type,constraint_matrix):   
+
     decomposed_result={}
     dec=Decomposer(method_name = method_name,method_type=method_type, constraint_matrix= constraint_matrix)
     decomposed=dec.getDecomposeResult()  
     decomposed_result["Q"]=decomposed[0]
     decomposed_result["R"]=decomposed[1]
+    
     return decomposed_result 
 
--- linear_systems/least_square.py
+++ linear_systems/least_square.py
@@ -1,8 +1,10 @@
 from .utils.least_square_utils import LeastSquares
 
-def get_least_squares(method_name,method_type,constraint_matrix,decomposed,rhs_matrix):   
+def get_least_squares(method_name,method_type,constraint_matrix,decomposed,rhs_matrix):  
+
     ls=LeastSquares(method_name = method_name,method_type=method_type,
                      constraint_matrix=constraint_matrix,decomposed=decomposed,rhs_matrix=rhs_matrix)
     least_squares=ls.getLeastSquares() 
     least_squares_norm=ls.getLeastSquaresNorm(least_squares) 
+    
     return least_squares,least_squares_norm
